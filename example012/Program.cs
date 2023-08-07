cb1bf32f5e05efcc01c795577b3923c7ac047f14
LitavrinaAA <a.a.litavrina@google.com>
2023-08-07 16:36:44 +0300

    ex012

---
+++ example012/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программуб которая выводит случайное число из отрезка [10,99] и 
+// и показывает наибольшую цифру числа
+
+//Например:
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+int MaxDigit(int num)
+{
+    //85/10 -> 8
+    //85%10 ->5
+    if (num / 10 > num % 10)
+        return num / 10;
+    else
+        return num % 10;
+}
+int number = new Random().Next(10,100);
+int res = MaxDigit(number);
+System.Console.WriteLine($"Число {number} максимальная цифра {res}");
