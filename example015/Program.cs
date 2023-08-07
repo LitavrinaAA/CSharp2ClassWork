14cd521b922e4ccd47bd9247da26d4a08118c82f
LitavrinaAA <a.a.litavrina@google.com>
2023-08-07 17:52:58 +0300

    ex015

---
+++ example015/Program.cs
@@ -0,0 +1,16 @@
+﻿// принимает на вход число и прверяет кратно ли оно одновременно 7 и 23
+int InputNum(string message)
+{
+    System.Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+bool Check723(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+int num = InputNum("Введите число: ");
+System.Console.WriteLine(Check723(num));
+
+
+
