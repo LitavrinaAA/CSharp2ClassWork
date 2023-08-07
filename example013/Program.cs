53669dff1ca5dfe6b8e0376d412bbbfd4d86e48b
LitavrinaAA <a.a.litavrina@google.com>
2023-08-07 16:50:07 +0300

    ex013

---
+++ example013/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выводит случайное трехзначное число и удаляет вторую цифру этого числа
+int num = new Random().Next(100,1000);
+//123 % 10 -> 3
+//12 / 100 -> 1
+// 1*10 + 3 = 13
+
+int DelSecondNum(int num){
+    int firstNum = num / 100;
+    int therdNum = num % 10;
+    return firstNum*10 + therdNum;
+}
+
+System.Console.WriteLine($"Число {num} -> " + DelSecondNum(num));
+
